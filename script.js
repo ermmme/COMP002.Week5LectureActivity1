e6dbdc91c8550f1c6ec363429513a3aad47ffaa1
ermmme <185312182+ermmme@users.noreply.github.com>
2024-11-13 21:43:55 -0800

    Completed second part of the activity.

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ myAverage += myGrades[i];
 myAverage = myAverage / myGrades.length;
 
 console.log("My grade average is " + myAverage);
+
+class InvalidInputError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidInputError";
+    }
+}
+
+function validateNumber(input) {
+    if (typeof input !== "number") {
+        throw new InvalidInputError("Input must be a number.");
+    }
+    if (input < 0 || input > 100) {
+        throw new InvalidInputError("Input must be between 0 and 100.");
+    }
+    console.log("Valid input: " + input);
+}
+
+try {
+    validateNumber(150);
+} catch (error) {
+    if (error instanceof InvalidInputError) {
+        console.error("Custom Exception Caught: " + error.message);
+    } else {
+        console.error("An unexpected error occurred: " + error.message);
+    }
+}
+
+try {
+    validateNumber(85);
+} catch (error) {
+    console.error("An unexpected error occurred: " + error.message);
+}
